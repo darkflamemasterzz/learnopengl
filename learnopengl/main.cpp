66be5895494600a2a186cdf8c8e038d1d5141f95
bin <m13678954738@gmail.com>
2019-11-04 08:52:44 +0800

    Getting Started -- Textures --mix

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -58,14 +58,14 @@ int main()
 	};
 
 	unsigned int VBO, VAO, EBO; // Vertex Buffer Object & Vertex Array Object & Element buffer object reference id
-	glGenVertexArrays(1, &VAO); // Generate a VAO
-	glGenBuffers(1, &VBO); // Generate a VBO
-	glGenBuffers(1, &EBO); // Generate a EBO
+	glGenVertexArrays(1, &VAO); 
+	glGenBuffers(1, &VBO); //？ creates buffer objects and returns the identifiers of the buffer objects.； Where is the object and what is the identifier?
+	glGenBuffers(1, &EBO); 
 
-	glBindVertexArray(VAO); // bind the VAO
+	glBindVertexArray(VAO); //? bind the VAO; Bind to what?
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind the vertex buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // copy our vertices array in a vertex buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //? bind the vertex buffer and instruct this data type ; Bind to what?
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //? copy the data in to buffer object;如果glBindBuffer和glBufferData的target参数不一样会怎样？
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // bind the element buffer
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // sizeof(indices)?
@@ -82,12 +82,14 @@ int main()
 
 	// load and create a texture
 	// -------------------------
-	unsigned int texture;
-	glGenTextures(1, &texture); //? unclear
-	glBindTexture(GL_TEXTURE_2D, texture); //? all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	unsigned int texture1, texture2;
+	// texture 1
+	// ----------
+	glGenTextures(1, &texture1); //? unclear
+	glBindTexture(GL_TEXTURE_2D, texture1); //? all upcoming GL_TEXTURE_2D operations now have effect on this texture object
 	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); //? set texture wrapping to GL_REPEAT
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT); //? set texture wrapping to GL_REPEAT
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 	// set texture filtering parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); //?
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); 
@@ -105,8 +107,35 @@ int main()
 		std::cout << "Failed to load texture" << std::endl;
 	}
 	stbi_image_free(data);
+	// texture 2
+	// ---------
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2); 
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set两次？一次过set好不行吗？
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load image, create texture and generate mipmaps
+	data = stbi_load("face.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 
+	// tell opengl for each sampler to which texture unit it belongs to
+	// sampler?
+	// ----------------------------------------------------------------
 	ourShader.use();
+	ourShader.setInt("texture1", 0);
+	ourShader.setInt("texture2", 1);
 
 	// set dimensions
 	glViewport(0, 0, 800, 600);
@@ -124,8 +153,11 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// bind Texture
-		glBindTexture(GL_TEXTURE_2D, texture); // bind texture again?
+		// bind Texture on corresponding texture units
+		glActiveTexture(GL_TEXTURE0); //?
+		glBindTexture(GL_TEXTURE_2D, texture1); // bind texture again?
+		glActiveTexture(GL_TEXTURE1); 
+		glBindTexture(GL_TEXTURE_2D, texture2); 
 
 		// Draw code in render loop
 		// --------------------------
@@ -170,6 +202,7 @@ void processInput(GLFWwindow* window)
 // 1.unresolved external: glfw3...
 	// glfw3.lib是x64格式的，与target machine的x86格式冲突了
 		// 什么是target machine?
+// 2.加载awesomeface.png和awesomeface.jpg出了事故，加载face.jpg却没什么毛病
 
 /*疑惑*/
 // 1.How to link vertex buffer to vertex shader?
@@ -178,6 +211,10 @@ void processInput(GLFWwindow* window)
 	// This vertex's data is represented using vertex attributes that can contain any data we'd like
 // 4.VAO? VBO?
 // 5.buffer? GPU memory?
-// 6.为什么能够unbind VAO却不能unbind VBO?
+// 6.为什么在VAO里面可以解绑VBO，却不能解绑EBO呢？
 	// http://www.photoneray.com/opengl-vao-vbo/
 // 7.顶点数据怎么传到shader里去？
+// 8.glGenBuffers()是客户端(cpu、内存端)的函数，而buffer指的是显卡存储空间里的一块缓存区，
+// 那么一个属于客户端的函数时怎么生成一个属于服务端的buffer的？
+	// glGenBuffers()生成的是一个Buffer的ID，而不是一个Buffer
+// 9.vertex shader接受到的数据是从哪里来的？
